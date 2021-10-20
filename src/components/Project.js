ce85cfd7db34ed937dd779575292ab59478489d2
Lauren Goss <laurenlgoss98@gmail.com>
2021-10-20 16:14:05 -0400

    passed props to project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,59 @@
 import React from 'react';
 
-function Project() {}
+function Project({
+  borderColor,
+  columnSide,
+  deployedApp,
+  description,
+  githubRepo,
+  imgSrc,
+  languages,
+  title,
+}) {
+  return (
+    <div
+      className={`card project-card${
+        borderColor === 'yellowGreen' ? ' royg-border' : ' biv-border'
+      }`}
+      style={{ width: 'auto;' }}
+    >
+      <div className="project-img-container">
+        <img className="card-img-top" src={imgSrc} alt="Card image cap" />
+        <div className="project-icons-container">
+          <a href={githubRepo} target="_blank">
+            <i className="fab fa-3x fa-github"></i>
+          </a>
+          {deployedApp ? (
+            <a href={deployedApp} target="_blank">
+              <i className="fas fa-3x fa-external-link-alt"></i>
+            </a>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+      <div className={`${columnSide === 'right' ? 'text-right' : ''}`}>
+        <h4 className="project-title">{title}</h4>
+        <p className="alt-font">{description}</p>
+        <ul
+          className={`fa-ul custom-list${
+            columnSide === 'right' ? ' text-right' : ' text-left'
+          }`}
+        >
+          {languages.map((language) => {
+            return (
+              <li className="alt-font">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                {language}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Project;
--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -2,6 +2,56 @@ import React from 'react';
 import FeaturedProject from './FeaturedProject';
 import Project from './Project';
 
+const skyGazer = {
+  borderColor: 'yellowGreen',
+  columnSide: 'left',
+  deployedApp: 'https://pamtheham.github.io/Sky-Gazer/',
+  description:
+    "// dashboard that uses the user's location and chosen satellite to determine the dates/times of that satellite's passovers for the next 7 days and their predicted visiblity.",
+  githubRepo: 'https://github.com/laurenlgoss/Sky-Gazer',
+  imgSrc: '',
+  languages: ['JavaScript', 'API', 'Bulma', 'Local Storage'],
+  title: 'Sky Gazer',
+};
+
+const employeeTracker = {
+  borderColor: 'bluePink',
+  columnSide: 'left',
+  deployedApp: '',
+  description:
+    '// backend JavaScript application that allows the user to interact with an employee database through the command line.',
+  githubRepo: 'https://github.com/laurenlgoss/employee-tracker',
+  imgSrc: '',
+  languages: ['JavaScript', 'Node.js', 'MySQL', 'Inquirer.js'],
+  title: 'Employee Tracker',
+};
+
+const weatherDashboard = {
+  borderColor: 'bluePink',
+  columnSide: 'right',
+  deployedApp: 'https://laurenlgoss.github.io/weather-dashboard/',
+  description:
+    '// weather dashboard that displays the current weather and 5-day forecast for whichever city user inputs.',
+  githubRepo: 'https://github.com/laurenlgoss/weather-dashboard',
+  imgSrc: '',
+  languages: ['JavaScript', 'API', 'Bootstrap', 'Local Storage'],
+  title: 'Weather Dashboard',
+};
+
+const codeQuiz = {
+  borderColor: 'yellowGreen',
+  columnSide: 'right',
+  deployedApp: 'https://laurenlgoss.github.io/code-quiz/',
+  description:
+    '// multiple choice quiz that tests the user on knowledge of HTML, CSS, and JavaScript concepts.',
+  githubRepo: 'https://github.com/laurenlgoss/code-quiz',
+  imgSrc: '',
+  languages: ['JavaScript', 'Local Storage', 'HTML/CSS'],
+  title: 'Code Quiz',
+};
+
+const projectsArray = [skyGazer, employeeTracker, weatherDashboard, codeQuiz];
+
 function ProjectsContainer() {
   return (
     <div id="projects">
@@ -9,17 +59,49 @@ function ProjectsContainer() {
         <h1 className="section-title" id="projects-title">
           projects
         </h1>
-        <FeaturedProject />
+        {/* <FeaturedProject /> */}
       </div>
 
       <div className="row justify-content-around projects">
         <div className="col-md-6">
-          <Project />
-          <Project />
+          {projectsArray
+            .filter((project) => {
+              return project.columnSide === 'left';
+            })
+            .map((project) => {
+              return (
+                <Project
+                  borderColor={project.borderColor}
+                  columnSide={project.columnSide}
+                  deployedApp={project.deployedApp}
+                  description={project.description}
+                  githubRepo={project.githubRepo}
+                  imgSrc={project.imgSrc}
+                  languages={project.languages}
+                  title={project.title}
+                />
+              );
+            })}
         </div>
         <div className="col-md-6">
-          <Project />
-          <Project />
+        {projectsArray
+            .filter((project) => {
+              return project.columnSide === 'right';
+            })
+            .map((project) => {
+              return (
+                <Project
+                  borderColor={project.borderColor}
+                  columnSide={project.columnSide}
+                  deployedApp={project.deployedApp}
+                  description={project.description}
+                  githubRepo={project.githubRepo}
+                  imgSrc={project.imgSrc}
+                  languages={project.languages}
+                  title={project.title}
+                />
+              );
+            })}
         </div>
       </div>
     </div>
