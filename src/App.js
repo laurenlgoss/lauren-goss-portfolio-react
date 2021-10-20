e769897fef353000871f7479409276504cd50d01
Lauren Goss <laurenlgoss98@gmail.com>
2021-10-20 12:40:02 -0400

    added outline of files

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Project from "./components/Project";
+
+function App() {}
+
+export default App;
