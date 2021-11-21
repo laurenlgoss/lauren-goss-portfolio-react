590811c15e8eb793af8b18d1432ac46c9fe08296
Lauren Goss <laurenlgoss98@gmail.com>
2021-11-21 17:56:59 -0500

    added skills carousel

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import Skills from './Skills';
+import SkillsCarousel from './SkillsCarousel';
+
+import javaScriptImg from '../images/skills-logos/javascript.png';
+import htmlImg from '../images/skills-logos/html-5.png';
+import cssImg from '../images/skills-logos/css.png';
+import jQueryImg from '../images/skills-logos/jquery.png';
+import reactImg from '../images/skills-logos/react.png';
+import nodeImg from '../images/skills-logos/node-js.png';
+import bootstrapImg from '../images/skills-logos/bootstrap.png';
+import expressImg from '../images/skills-logos/express.png';
+import mySQLImg from '../images/skills-logos/mysql.png';
+import sequelizeImg from '../images/skills-logos/sequelize.png';
+import mongodbImg from '../images/skills-logos/mongodb.png';
+import netImg from '../images/skills-logos/microsoft-dotnet.png';
+import handlebarsImg from '../images/skills-logos/handlebars.png';
+import graphqlImg from '../images/skills-logos/graphql.png';
+import gitImg from '../images/skills-logos/git.png';
+import visualStudioCodeImg from '../images/skills-logos/visual-studio-code.png';
+import jestImg from '../images/skills-logos/jest.png';
+import figmaImg from '../images/skills-logos/figma.png';
+import bitbucketImg from '../images/skills-logos/bitbucket.png';
+import sourcetreeImg from '../images/skills-logos/sourcetree.png';
+import githubImg from '../images/skills-logos/github.png';
 
 const frontendSkills = {
   category: 'frontend',
@@ -39,6 +62,165 @@ const otherSkills = {
 
 const skillsArray = [frontendSkills, backendSkills, otherSkills];
 
+const javaScript = {
+  img: javaScriptImg,
+  name: 'JavaScript',
+};
+
+const html = {
+  img: htmlImg,
+  name: 'HTML5',
+};
+
+const css = {
+  img: cssImg,
+  name: 'CSS3',
+};
+
+const jQuery = {
+  img: jQueryImg,
+  name: 'jQuery',
+};
+
+const react = {
+  img: reactImg,
+  name: 'ReactJS',
+};
+
+const node = {
+  img: nodeImg,
+  name: 'Node.js',
+};
+
+const bootstrap = {
+  img: bootstrapImg,
+  name: 'Bootstrap',
+};
+
+const materialUI = {
+  img: '',
+  name: 'Material UI',
+};
+
+const express = {
+  img: expressImg,
+  name: 'Express.js',
+};
+
+const mySQL = {
+  img: mySQLImg,
+  name: 'MySQL',
+};
+
+const sequelize = {
+  img: sequelizeImg,
+  name: 'Sequelize',
+};
+
+const mongodb = {
+  img: mongodbImg,
+  name: 'MongoDB',
+};
+
+const net = {
+  img: netImg,
+  name: '.NET',
+};
+
+const visualBasic = {
+  img: '',
+  name: 'Visual Basic'
+}
+
+const handlebars = {
+  img: handlebarsImg,
+  name: 'Handlebars.js',
+};
+
+const graphql = {
+  img: graphqlImg,
+  name: 'GraphQL',
+};
+
+const apollo = {
+  img: '',
+  name: 'Apollo',
+};
+
+const git = {
+  img: gitImg,
+  name: 'Git',
+};
+
+const visualStudioCode = {
+  img: visualStudioCodeImg,
+  name: 'Visual Studio Code',
+};
+
+const jest = {
+  img: jestImg,
+  name: 'Jest',
+};
+
+const figma = {
+  img: figmaImg,
+  name: 'Figma',
+};
+
+const bitbucket = {
+  img: bitbucketImg,
+  name: 'Bitbucket',
+};
+
+const sourcetree = {
+  img: sourcetreeImg,
+  name: 'Sourcetree',
+};
+
+const robo3T = {
+  img: '',
+  name: 'Robo 3T',
+};
+
+const insomnia = {
+  img: '',
+  name: 'Insomnia',
+};
+
+const github = {
+  img: githubImg,
+  name: 'GitHub',
+};
+
+const carouselSkillsArray = [
+  // apollo,
+  bitbucket,
+  bootstrap,
+  css,
+  express,
+  figma,
+  git,
+  github,
+  graphql,
+  handlebars,
+  html,
+  // insomnia,
+  javaScript,
+  jest,
+  jQuery,
+  // materialUI,
+  mongodb,
+  mySQL,
+  net,
+  node,
+  react,
+  // robo3T,
+  sequelize,
+  sourcetree,
+  // visualBasic,
+  visualStudioCode,
+];
+
 function AboutMe() {
   return (
     <div id="about-me">
@@ -54,8 +236,8 @@ function AboutMe() {
             veterinary field following graduation, I was ready for a career
             change and began teaching myself Swift in Summer of 2020. Shortly
             after, I discovered a passion for logic and enrolled in UNC's Coding
-            Boot Camp, from which I recently graduated. I have a
-            particular enthusiasm for writing frontend and backend logic.
+            Boot Camp, from which I recently graduated. I have a particular
+            enthusiasm for writing frontend and backend logic.
           </p>
         </div>
       </div>
@@ -71,6 +253,7 @@ function AboutMe() {
           );
         })}
       </div>
+      <SkillsCarousel skills={carouselSkillsArray} />
     </div>
   );
 }
---
+++ src/components/SkillsCarousel.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+
+function SkillsCarousel({ skills }) {
+  const sliderItems = skills.length > 6 ? 6 : skills.length;
+  const items = [];
+
+  for (let i = 0; i < skills.length; i += sliderItems) {
+    if (i % sliderItems === 0) {
+      items.push(
+        <div className="row">
+          {skills.slice(i, i + sliderItems).map((skill, i) => {
+            return (
+              <div className="col">
+                <div className="text-center" key={i}>
+                  <img src={skill.img} />
+                  <div className="mt-3 alt-font">{skill.name}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="carousel row">
+      <div className="col">
+        <Carousel
+          animation={'slide'}
+          duration={7000}
+          indicators={false}
+          interval={8000}
+        >
+          {items}
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
+export default SkillsCarousel;
